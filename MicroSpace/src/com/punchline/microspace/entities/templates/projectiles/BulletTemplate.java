8bb1e883a05183ef7e49a57bc8c019c30e4d6e1a
William Guss <carlostnb@gmail.com>
2013-07-24 11:46:46 -0600

    BulletTemplate is completed along with the required sprites.

--- MicroSpace/src/com/punchline/microspace/entities/templates/projectiles/BulletTemplate.java
+++ MicroSpace/src/com/punchline/microspace/entities/templates/projectiles/BulletTemplate.java
@@ -3,6 +3,7 @@
  */
 package com.punchline.microspace.entities.templates.projectiles;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
@@ -14,6 +15,7 @@ import com.punchline.javalib.entities.components.Bullet;
 import com.punchline.javalib.entities.components.Health;
 import com.punchline.javalib.entities.components.physical.Collidable;
 import com.punchline.javalib.entities.components.physical.Particle;
+import com.punchline.javalib.entities.components.render.Renderable;
 import com.punchline.javalib.entities.components.render.Sprite;
 import com.punchline.javalib.utils.Convert;
 
@@ -22,8 +24,23 @@ import com.punchline.javalib.utils.Convert;
  * @created Jul 23, 2013
  */
 public class BulletTemplate implements EntityTemplate {
-	private Texture sheetTexture;
-	private TextureRegion framesRegion;
+	private Texture bulletTex;
+	private TextureRegion[] bulletRect;
+	
+	/**
+	 * Constructs the bullet template for use with bullets.
+	 */
+	public BulletTemplate(){
+		bulletTex = new Texture(Gdx.files.internal("data/assets/Textures/bulletSprites.png"));
+		bulletRect = new TextureRegion[]{
+			new TextureRegion(bulletTex, 0, 1, 2, 1), //Blue
+			new TextureRegion(bulletTex, 0, 0, 2, 1), //Red
+			new TextureRegion(bulletTex, 2, 0, 2, 1), //Yellow
+			new TextureRegion(bulletTex, 2, 1, 2, 1), //Purple
+			new TextureRegion(bulletTex, 0, 2, 4, 2)  //Big
+		};
+	}
+	
 	
 	/** {@inheritDoc}
 	 * @see com.punchline.javalib.entities.EntityTemplate#buildEntity(com.punchline.javalib.entities.Entity, com.punchline.javalib.entities.EntityWorld, java.lang.Object[])
@@ -55,49 +72,51 @@ public class BulletTemplate implements EntityTemplate {
 		
 		//OnCollide
 		Collidable col = e.addComponent(Collidable.class,
-				new Collidable(){
-					@Override
-					public void onAdd(ComponentManager container) {
-					}
-
-					@Override
-					public void onRemove(ComponentManager container) {	
-					}
-
-					@Override
-					public float onCollide(Entity container, Entity victim) {
-						Bullet b = container.getComponent();
-						
-						//If the bullet hits the firer, continue firing the bullet.
-						if(victim == b.getFirer()) 
-							return 1;
-						
-						else{
-							//IF THE VICTIM HAS HEALTH! RAWR
-							if(victim.hasComponent(Health.class))
-							{
-								Health h = victim.getComponent();
-								h.setHealth(h.getHealth()-b.getDamage());
-							}
-							
-							container.delete();
-							//TERMINATE THE RAY CAST
-							return 0;
+			new Collidable(){
+				@Override
+				public void onAdd(ComponentManager container) {
+				}
+	
+				@Override
+				public void onRemove(ComponentManager container) {	
+				}
+	
+				@Override
+				public float onCollide(Entity container, Entity victim) {
+					Bullet b = container.getComponent();
+					
+					//If the bullet hits the firer, continue firing the bullet.
+					if(victim == b.getFirer()) 
+						return 1;
+					
+					else{
+						//IF THE VICTIM HAS HEALTH! RAWR
+						if(victim.hasComponent(Health.class))
+						{
+							Health h = victim.getComponent();
+							h.setHealth(h.getHealth()-b.getDamage());
 						}
+						
+						container.delete();
+						//TERMINATE THE RAY CAST
+						return 0;
 					}
-			
-		});
+				}
+			});
 		
 		
 		
-		//Sprite TODO: Write sprite code!
-		Sprite s;
-		if(type.equalsIgnoreCase("blue")){
-			
-		}
-		else if(type.equalsIgnoreCase("red")){
-			
-		}
+		//Sprite TODO: Consider sprite pooling.
+		if(type.equalsIgnoreCase("blue"))
+			e.addComponent(Renderable.class, new Sprite(bulletTex, bulletRect[0]));
+		else if(type.equalsIgnoreCase("red"))
+			e.addComponent(Renderable.class, new Sprite(bulletTex, bulletRect[1]));
+		else if(type.equalsIgnoreCase("yellow"))
+			e.addComponent(Renderable.class, new Sprite(bulletTex, bulletRect[2]));
+		else if(type.equalsIgnoreCase("purple"))
+			e.addComponent(Renderable.class, new Sprite(bulletTex, bulletRect[3]));
+		else if(type.equalsIgnoreCase("big"))
+			e.addComponent(Renderable.class, new Sprite(bulletTex, bulletRect[4]));
 		
 		return e;
 	}
