1c36f51ab54a527c5985b34736f4a43fee8e39ae
William Guss <carlostnb@gmail.com>
2013-07-27 11:04:01 -0600

    Replacing MookSpawnSystem with a generic EntitySpawner system from JavaLib

--- MicroSpace/src/com/punchline/microspace/entities/SpaceWorld.java
+++ MicroSpace/src/com/punchline/microspace/entities/SpaceWorld.java
@@ -10,7 +10,6 @@ import com.punchline.javalib.entities.EntityWorld;
 import com.punchline.javalib.entities.systems.generic.TrackingCameraSystem;
 import com.punchline.microspace.Worlds;
 import com.punchline.microspace.entities.systems.AsteroidSpawnSystem;
-import com.punchline.microspace.entities.systems.MookSpawnSystem;
 import com.punchline.microspace.entities.systems.PlayerControlSystem;
 import com.punchline.microspace.entities.templates.AsteroidTemplate;
 import com.punchline.microspace.entities.templates.MookTemplate;
@@ -70,7 +69,6 @@ public class SpaceWorld extends EntityWorld {
 		
 		//Spawning
 		systems.addSystem(new AsteroidSpawnSystem());
-		systems.addSystem(new MookSpawnSystem());
 	}
 
 	/**
